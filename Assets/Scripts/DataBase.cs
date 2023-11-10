f2a3fca105ca60ccf29a8733d39407ed59626a5b
EduENTI <e@s.com>
2023-11-10 20:59:15 +0100

    Clase plant y buscar datos en la BD

--- Assets/Scripts/DataBase.cs
+++ Assets/Scripts/DataBase.cs
@@ -10,12 +10,12 @@ public class DataBase : MonoBehaviour
     IDbConnection conn;
 
     string dbName = "entifarm.db";
+
     void Start()
     {
         conn = new SqliteConnection(string.Format("URI=file:{0}", dbName));
     }
 
-    // Update is called once per frame
     void Update()
     {
         
